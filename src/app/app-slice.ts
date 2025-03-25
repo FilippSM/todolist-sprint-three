8c5a1b2d65034b2c25351865dd62ce6fb7c909c1
Filipp Sam <philippsm666@gmail.com>
2025-03-25 23:03:31 +0300

    (sprint 4 week 1) add appSlice

---
+++ src/app/app-slice.ts
@@ -0,0 +1,28 @@
+import {createSlice } from "@reduxjs/toolkit"
+
+//три обязательных параметра
+export const appSlice = createSlice({
+  name: "app",
+  initialState: {
+    themeMode: "light" as ThemeMode,
+  },
+  reducers: (create) => ({
+      //подъредьюсер или экш криэйтор
+      changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+        state.themeMode = action.payload.themeMode
+      }),
+  }),
+})
+
+export const appReducer = appSlice.reducer
+export const {changeThemeModeAC} = appSlice.actions
+
+export type ThemeMode = "dark" | "light"
+
+/* export const changeThemeModeAC = createAction<{ themeMode: ThemeMode }>("app/changeThemeMode")
+
+export const appReducer = createReducer(initialState, (builder) => {
+  builder.addCase(changeThemeModeAC, (state, action) => {
+    state.themeMode = action.payload.themeMode
+  })
+}) */
